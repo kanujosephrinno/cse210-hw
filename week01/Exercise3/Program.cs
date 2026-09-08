05ba20c8aac645539351b7426c55e9bc19293bf7
Joseph Rinno Kanu <kanujosephrinno@gmail.com>
2026-09-08 15:41:15 +0000

    Complete Excercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        Console.Write("What is your guess? ");
+        string guessInput = Console.ReadLine();
+        int guess = int.Parse(guessInput);
+
+        while (guess != magicNumber)
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.Write("What is your guess? ");
+            guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
